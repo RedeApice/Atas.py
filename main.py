8b5b9123f3a9572468eea804fac628492e3699e8
RedeApice <infra@redeapice.com.br>
2025-01-08 15:16:30 -0300

    Atualização para o path

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np
 import easyocr
 
-image_file_name = "colocar_imagem(_precisa_estar_na_pasta)"
+image_file_name = r"C:\Users\Dell\Downloads\Atas\Atas.py\scan_20241220102933 ok.jpeg"
 image = cv2.imread(image_file_name)
 
 if image is None:
     print("Erro ao carregar a imagem.")
 else:
+    bright_image = cv2.convertScaleAbs(image, alpha=1, beta=50)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
     sharpened = cv2.filter2D(gray, -1, sharpen_kernel)
